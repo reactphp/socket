8022ba50758550e3538bbe4f1f0de785c31616b9
Christian Lück <christian@lueck.tv>
2014-09-06 18:02:25 +0200

    Move example client into a test case

---
+++ tests/SecureConnectorTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace React\Tests\SocketClient;
+
+use React\EventLoop\Factory as LoopFactory;
+use React\Socket\Server;
+use React\Dns\Resolver\Factory as DnsFactory;
+use React\SocketClient\Connector;
+use React\SocketClient\SecureConnector;
+use React\Stream\Stream;
+use Clue\React\Block;
+
+class SecureConnectorTest extends TestCase
+{
+    public function testA()
+    {
+        $loop = LoopFactory::create();
+
+        $receivedServer = '';
+        $receivedClient = '';
+        $connected = 0;
+
+        $server = new Server($loop);
+        $server->on('connection', function (Stream $stream) use (&$receivedServer, &$connected) {
+            $connected++;
+
+            // $stream->pipe($stream);
+            $stream->on('data', function ($data) use ($stream, &$receivedServer) {
+                $receivedServer .= $data;
+                $stream->write($data);
+            });
+        });
+        $server->listen(6000);
+
+        $dnsResolverFactory = new DnsFactory();
+        $resolver = $dnsResolverFactory->createCached('8.8.8.8', $loop);
+
+        $connector = new Connector($loop, $resolver);
+        $secureConnector = new SecureConnector($connector, $loop);
+
+        $promise = $secureConnector->create('127.0.0.1', 6001);
+        //$promise = $connector->create('127.0.0.1', 6000);
+        $client = Block\await($promise, $loop);
+        /* @var $client Stream */
+
+        while (!$connected) {
+            $loop->tick();
+        }
+
+        $client->on('data', function ($data) use (&$receivedClient) {
+            $receivedClient .= $data;
+        });
+
+        $this->assertEquals('', $receivedServer);
+        $this->assertEquals('', $receivedClient);
+
+        $echo = function ($str) use (&$receivedClient, &$receivedClient, $loop, $client) {
+            $receivedClient = $receivedServer = '';
+            $client->write($str);
+            while ($receivedClient !== $str) {
+                $loop->tick();
+            }
+        };
+
+        $echo('hello');
+
+        $echo('world');
+
+        // send a 10k message once to fill buffer (failing!)
+        //$echo(str_repeat('1234567890', 10000));
+
+        $echo('again');
+    }
+}
