a072b05dc0552e59ca6c2afadc20b652b8574aaa
Christian Lück <christian@lueck.tv>
2016-03-09 00:38:26 +0100

    Rely on environment variables to run TLS integration test

--- tests/SecureIntegrationTest.php
+++ tests/SecureIntegrationTest.php
@@ -4,14 +4,26 @@ namespace React\Tests\SocketClient;
 
 use React\EventLoop\Factory as LoopFactory;
 use React\Socket\Server;
-use React\Dns\Resolver\Factory as DnsFactory;
-use React\SocketClient\Connector;
+use React\SocketClient\TcpConnector;
 use React\SocketClient\SecureConnector;
 use React\Stream\Stream;
 use Clue\React\Block;
 
-class SecureConnectorTest extends TestCase
+class SecureIntegrationTest extends TestCase
 {
+    private $portSecure;
+    private $portPlain;
+
+    public function setUp()
+    {
+        $this->portSecure = getenv('TEST_SECURE');
+        $this->portPlain = getenv('TEST_PLAIN');
+
+        if ($this->portSecure === false || $this->portPlain === false) {
+            $this->markTestSkipped('Needs TEST_SECURE=X and TEST_PLAIN=Y environment variables to run, see README.md');
+        }
+    }
+
     public function testA()
     {
         $loop = LoopFactory::create();
@@ -30,30 +42,11 @@ class SecureConnectorTest extends TestCase
                 $stream->write($data);
             });
         });
-        $server->listen(6000);
+        $server->listen($this->portPlain);
 
-        $dnsResolverFactory = new DnsFactory();
-        $resolver = $dnsResolverFactory->createCached('8.8.8.8', $loop);
+        $connector = new SecureConnector(new TcpConnector($loop), $loop, array('verify_peer' => false));
 
-        $connector = new Connector($loop, $resolver);
-
-        // verify server is listening by creating an unencrypted connection once
-        $promise = $connector->create('127.0.0.1', 6001);
-        try {
-            $client = Block\await($promise, $loop);
-            /* @var $client Stream */
-            $client->close();
-        } catch (\Exception $e) {
-            $this->markTestSkipped('stunnel not reachable?');
-        }
-
-        $this->assertEquals(0, $connected);
-
-        $secureConnector = new SecureConnector($connector, $loop, array('verify_peer' => false));
-
-        $promise = $secureConnector->create('127.0.0.1', 6001);
-        //$promise = $connector->create('127.0.0.1', 6000);
-        $client = Block\await($promise, $loop);
+        $client = Block\await($connector->create('127.0.0.1', $this->portSecure), $loop);
         /* @var $client Stream */
 
         while (!$connected) {
@@ -79,7 +72,7 @@ class SecureConnectorTest extends TestCase
 
         $echo('world');
 
-        // send a 10k message once to fill buffer (failing!)
+        // send a 10k message once to fill buffer
         $echo(str_repeat('1234567890', 10000));
 
         $echo('again');
