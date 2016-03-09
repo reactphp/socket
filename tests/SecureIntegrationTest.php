e5bf902d82526e67acccd146b1655a4b5ad91f9e
Christian Lück <christian@lueck.tv>
2016-03-09 23:41:05 +0100

    Break up into smaller, independent test cases

--- tests/SecureIntegrationTest.php
+++ tests/SecureIntegrationTest.php
@@ -8,11 +8,19 @@ use React\SocketClient\TcpConnector;
 use React\SocketClient\SecureConnector;
 use React\Stream\Stream;
 use Clue\React\Block;
+use React\Promise\Promise;
+use Evenement\EventEmitterInterface;
+use React\Promise\Deferred;
+use React\Stream\BufferedSink;
 
 class SecureIntegrationTest extends TestCase
 {
     private $portSecure;
     private $portPlain;
+
+    private $loop;
+    private $server;
+    private $connector;
 
     public function setUp()
     {
@@ -22,59 +30,122 @@ class SecureIntegrationTest extends TestCase
         if ($this->portSecure === false || $this->portPlain === false) {
             $this->markTestSkipped('Needs TEST_SECURE=X and TEST_PLAIN=Y environment variables to run, see README.md');
         }
+
+        $this->loop = LoopFactory::create();
+        $this->server = new Server($this->loop);
+        $this->server->listen($this->portPlain);
+        $this->connector = new SecureConnector(new TcpConnector($this->loop), $this->loop, array('verify_peer' => false));;
     }
 
-    public function testA()
+    public function tearDown()
     {
-        $loop = LoopFactory::create();
+        if ($this->server !== null) {
+            $this->server->shutdown();
+            $this->server = null;
+        }
+    }
 
-        $receivedServer = '';
-        $receivedClient = '';
-        $connected = 0;
-
-        $server = new Server($loop);
-        $server->on('connection', function (Stream $stream) use (&$receivedServer, &$connected) {
-            $connected++;
-
-            // $stream->pipe($stream);
-            $stream->on('data', function ($data) use ($stream, &$receivedServer) {
-                $receivedServer .= $data;
-                $stream->write($data);
-            });
-        });
-        $server->listen($this->portPlain);
-
-        $connector = new SecureConnector(new TcpConnector($loop), $loop, array('verify_peer' => false));
-
-        $client = Block\await($connector->create('127.0.0.1', $this->portSecure), $loop);
+    public function testConnectToServer()
+    {
+        $client = Block\await($this->connector->create('127.0.0.1', $this->portSecure), $this->loop);
         /* @var $client Stream */
 
-        while (!$connected) {
-            $loop->tick();
-        }
+        $client->close();
+    }
 
-        $client->on('data', function ($data) use (&$receivedClient) {
-            $receivedClient .= $data;
+    public function testConnectToServerEmitsConnection()
+    {
+        $promiseServer = $this->createPromiseForEvent($this->server, 'connection', $this->expectCallableOnce());
+
+        $promiseClient = $this->connector->create('127.0.0.1', $this->portSecure);
+
+        list($_, $client) = Block\awaitAll(array($promiseServer, $promiseClient), $this->loop);
+        /* @var $client Stream */
+
+        $client->close();
+    }
+
+    public function testSendSmallDataToServerReceivesOneChunk()
+    {
+        // server expects one connection which emits one data event
+        $receiveOnce = $this->expectCallableOnceWith('hello');
+        $this->server->on('connection', function (Stream $peer) use ($receiveOnce) {
+            $peer->on('data', $receiveOnce);
         });
 
-        $this->assertEquals('', $receivedServer);
-        $this->assertEquals('', $receivedClient);
+        $client = Block\await($this->connector->create('127.0.0.1', $this->portSecure), $this->loop);
+        /* @var $client Stream */
 
-        $echo = function ($str) use (&$receivedClient, &$receivedClient, $loop, $client) {
-            $receivedClient = $receivedServer = '';
-            $client->write($str);
-            while ($receivedClient !== $str) {
-                $loop->tick();
-            }
-        };
+        $client->write('hello');
 
-        $echo('hello');
+        Block\sleep(0.01, $this->loop);
 
-        $echo('world');
+        $client->close();
+    }
 
-        // send a 10k message once to fill buffer
-        $echo(str_repeat('1234567890', 10000));
+    public function testSendDataWithEndToServerReceivesAllData()
+    {
+        $disconnected = new Deferred();
+        $this->server->on('connection', function (Stream $peer) use ($disconnected) {
+            $received = '';
+            $peer->on('data', function ($chunk) use (&$received) {
+                $received .= $chunk;
+            });
+            $peer->on('close', function () use (&$received, $disconnected) {
+                $disconnected->resolve($received);
+            });
+        });
 
-        $echo('again');
+        $client = Block\await($this->connector->create('127.0.0.1', $this->portSecure), $this->loop);
+        /* @var $client Stream */
+
+        $data = str_repeat('a', 200000);
+        $client->end($data);
+
+        // await server to report connection "close" event
+        $received = Block\await($disconnected->promise(), $this->loop);
+
+        $this->assertEquals($data, $received);
+    }
+
+    public function testConnectToServerWhichSendsSmallDataReceivesOneChunk()
+    {
+        $this->server->on('connection', function (Stream $peer) {
+            $peer->write('hello');
+        });
+
+        $client = Block\await($this->connector->create('127.0.0.1', $this->portSecure), $this->loop);
+        /* @var $client Stream */
+
+        $client->on('data', $this->expectCallableOnceWith('hello'));
+
+        Block\sleep(0.01, $this->loop);
+
+        $client->close();
+    }
+
+    public function testConnectToServerWhichSendsDataWithEndReceivesAllData()
+    {
+        $data = str_repeat('b', 100000);
+        $this->server->on('connection', function (Stream $peer) use ($data) {
+            $peer->end($data);
+        });
+
+        $client = Block\await($this->connector->create('127.0.0.1', $this->portSecure), $this->loop);
+        /* @var $client Stream */
+
+        // await data from client until it closes
+        $received = Block\await(BufferedSink::createPromise($client), $this->loop);
+
+        $this->assertEquals($data, $received);
+    }
+
+    private function createPromiseForEvent(EventEmitterInterface $emitter, $event, $fn)
+    {
+        return new Promise(function ($resolve) use ($emitter, $event, $fn) {
+            $emitter->on($event, function () use ($resolve, $fn) {
+                $resolve(call_user_func_array($fn, func_get_args()));
+            });
+        });
     }
 }
