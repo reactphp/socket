4e002715076f5c685e995ecec9cba4d3aa32e616
Christian Lück <christian@lueck.tv>
2017-01-01 19:58:17 +0100

    Add functional tests for sending/receiving large chunks

--- tests/FunctionalSecureServerTest.php
+++ tests/FunctionalSecureServerTest.php
@@ -9,6 +9,7 @@ use Clue\React\Block;
 use React\Socket\SecureServer;
 use React\SocketClient\SecureConnector;
 use React\Stream\Stream;
+use React\Socket\ConnectionInterface;
 
 class FunctionalSecureServerTest extends TestCase
 {
@@ -39,6 +40,170 @@ class FunctionalSecureServerTest extends TestCase
         $promise = $connector->create('127.0.0.1', $port);
 
         Block\await($promise, $loop, self::TIMEOUT);
+    }
+
+    public function testWritesDataToConnection()
+    {
+        $loop = Factory::create();
+
+        $server = new Server($loop);
+        $server = new SecureServer($server, $loop, array(
+            'local_cert' => __DIR__ . '/../examples/localhost.pem'
+        ));
+        $server->on('connection', $this->expectCallableOnce());
+        $server->listen(0);
+        $port = $server->getPort();
+
+        $server->on('connection', function (ConnectionInterface $conn) {
+            $conn->write('foo');
+        });
+
+        $connector = new SecureConnector(new TcpConnector($loop), $loop, array(
+            'verify_peer' => false
+        ));
+        $promise = $connector->create('127.0.0.1', $port);
+
+        $local = Block\await($promise, $loop, self::TIMEOUT);
+        /* @var $local React\Stream\Stream */
+
+        $local->on('data', $this->expectCallableOnceWith('foo'));
+
+        Block\sleep(self::TIMEOUT, $loop);
+    }
+
+    public function testWritesDataInMultipleChunksToConnection()
+    {
+        $loop = Factory::create();
+
+        $server = new Server($loop);
+        $server = new SecureServer($server, $loop, array(
+            'local_cert' => __DIR__ . '/../examples/localhost.pem'
+        ));
+        $server->on('connection', $this->expectCallableOnce());
+        $server->listen(0);
+        $port = $server->getPort();
+
+
+        $server->on('connection', function (ConnectionInterface $conn) {
+            $conn->write(str_repeat('*', 400000));
+        });
+
+        $connector = new SecureConnector(new TcpConnector($loop), $loop, array(
+            'verify_peer' => false
+        ));
+        $promise = $connector->create('127.0.0.1', $port);
+
+        $local = Block\await($promise, $loop, self::TIMEOUT);
+        /* @var $local React\Stream\Stream */
+
+        $received = 0;
+        $local->on('data', function ($chunk) use (&$received) {
+            $received += strlen($chunk);
+        });
+
+        Block\sleep(self::TIMEOUT, $loop);
+
+        $this->assertEquals(400000, $received);
+    }
+
+    public function testEmitsDataFromConnection()
+    {
+        $loop = Factory::create();
+
+        $server = new Server($loop);
+        $server = new SecureServer($server, $loop, array(
+            'local_cert' => __DIR__ . '/../examples/localhost.pem'
+        ));
+        $server->on('connection', $this->expectCallableOnce());
+        $server->listen(0);
+        $port = $server->getPort();
+
+        $once = $this->expectCallableOnceWith('foo');
+        $server->on('connection', function (ConnectionInterface $conn) use ($once) {
+            $conn->on('data', $once);
+        });
+
+        $connector = new SecureConnector(new TcpConnector($loop), $loop, array(
+            'verify_peer' => false
+        ));
+        $promise = $connector->create('127.0.0.1', $port);
+
+        $local = Block\await($promise, $loop, self::TIMEOUT);
+        /* @var $local React\Stream\Stream */
+
+        $local->write("foo");
+
+        Block\sleep(self::TIMEOUT, $loop);
+    }
+
+    public function testEmitsDataInMultipleChunksFromConnection()
+    {
+        $loop = Factory::create();
+
+        $server = new Server($loop);
+        $server = new SecureServer($server, $loop, array(
+            'local_cert' => __DIR__ . '/../examples/localhost.pem'
+        ));
+        $server->on('connection', $this->expectCallableOnce());
+        $server->listen(0);
+        $port = $server->getPort();
+
+        $received = 0;
+        $server->on('connection', function (ConnectionInterface $conn) use (&$received) {
+            $conn->on('data', function ($chunk) use (&$received) {
+                $received += strlen($chunk);
+            });
+        });
+
+        $connector = new SecureConnector(new TcpConnector($loop), $loop, array(
+            'verify_peer' => false
+        ));
+        $promise = $connector->create('127.0.0.1', $port);
+
+        $local = Block\await($promise, $loop, self::TIMEOUT);
+        /* @var $local React\Stream\Stream */
+
+        $local->write(str_repeat('*', 400000));
+
+        Block\sleep(self::TIMEOUT, $loop);
+
+        $this->assertEquals(400000, $received);
+    }
+
+    public function testPipesDataBackInMultipleChunksFromConnection()
+    {
+        $loop = Factory::create();
+
+        $server = new Server($loop);
+        $server = new SecureServer($server, $loop, array(
+            'local_cert' => __DIR__ . '/../examples/localhost.pem'
+        ));
+        $server->on('connection', $this->expectCallableOnce());
+        $server->listen(0);
+        $port = $server->getPort();
+
+        $server->on('connection', function (ConnectionInterface $conn) use (&$received) {
+            $conn->pipe($conn);
+        });
+
+        $connector = new SecureConnector(new TcpConnector($loop), $loop, array(
+            'verify_peer' => false
+        ));
+        $promise = $connector->create('127.0.0.1', $port);
+
+        $local = Block\await($promise, $loop, self::TIMEOUT);
+        /* @var $local React\Stream\Stream */
+
+        $received = 0;
+        $local->on('data', function ($chunk) use (&$received) {
+            $received += strlen($chunk);
+        });
+
+        $local->write(str_repeat('*', 400000));
+
+        Block\sleep(self::TIMEOUT, $loop);
+
+        $this->assertEquals(400000, $received);
     }
 
     public function testEmitsConnectionForNewConnectionWithEncryptedCertificate()
